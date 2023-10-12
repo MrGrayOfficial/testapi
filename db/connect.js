c594e7e9672d4704da8434a50ebf2868f98ec570
Akash <akash.kumar.2000201@gmail.com>
2023-10-12 20:43:57 +0530

    second commit

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://aksky2000:i8ytFRwsp5VsX0lw@cluster0.ektwjje.mongodb.net/?retryWrites=true&w=majority"
 
 const connectDB = () => {
     console.log("Connect Database...");
